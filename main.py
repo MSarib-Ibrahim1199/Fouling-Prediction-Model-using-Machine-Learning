df3a772e5d9643ab313cc06425360f2658951239
Muhammad Sarib Ibrahim <127873067+MSarib-Ibrahim1199@users.noreply.github.com>
2025-03-25 13:28:16 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,8 @@ from data_preprocessing import load_and_preprocess_data
 from train import train_models
 from evaluate import evaluate_models
 
-# Load dataset and preprocess it
 X_train, X_test, y_train, y_test = load_and_preprocess_data()
 
-# Train all models
 models, predictions = train_models(X_train, X_test, y_train, y_test)
 
-# Evaluate and visualize results
 evaluate_models(models, predictions, y_test)
